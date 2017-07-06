ae38c389efcc5de80a5c6b51fbed507bc4ff159b
bipinkh <bipinkh91@gmail.com>
2017-07-06 09:43:38 +0545

    added image display

--- ui.py
+++ ui.py
@@ -12,9 +12,11 @@ class LeafRecognition:
 		master.title("Plant Leaf Recognition")
 
 		#variables and constants
-		self.filename=" Select the file clicking above button."
+		self.filename="Select an image"
+		defaultimage="C:/Users/bipin/Downloads/cc.jpg"
 		self.selectedFileName=StringVar()
 		self.selectedFileName.set(self.filename)
+		
 
 		#dimensions
 		tab_height=300
@@ -22,41 +24,52 @@ class LeafRecognition:
 
 		#tabs
 		tabs = ttk.Notebook(root)
-		tab1 = ttk.Frame(tabs, width=tab_width, height=tab_height)
+		self.tab1 = ttk.Frame(tabs, width=tab_width, height=tab_height)
 		tab2 = ttk.Frame(tabs, width=tab_width, height=tab_height)
 		tab3 = ttk.Frame(tabs, width=tab_width, height=tab_height)
-
-		tabs.add(tab1, text='Home')
+		tabs.add(self.tab1, text='Home')
 		tabs.add(tab2, text='Databases')
 		tabs.add(tab3, text='Help')
-		tabs.grid(column=0)
 
 		#button and label for selecting image
-		selectedFileNameLabel = Label(tab1,textvariable=self.selectedFileName)
-		selectImage = Button(tab1, text="Check Image", command=self.selectfile)
+		selectedFileNameLabel = Label(self.tab1,textvariable=self.selectedFileName)
+		selectImage = Button(self.tab1, text="Select Image", command=self.selectfile)
+		previewImage = Button(self.tab1, text="Preview Image", command=self.displayimage)
 		#closebutton
-		closeButton = Button(tab1, text = "Quit", command=master.quit)
+		closeButton = Button(self.tab1, text = "Quit", command=master.quit)
 		closeButton.pack()
-
-
+		#image
+		img1=Image.open(defaultimage)
+		img1=img1.resize((120,200), Image.ANTIALIAS)
+		img1 = ImageTk.PhotoImage(img1)
+		self.imagePanel = ttk.Label(self.tab1,image=img1)
+		self.imagePanel.image=img1
 
 		#layout
+		tabs.grid(column=0)
 		selectImage.pack()
+		previewImage.pack()
 		selectedFileNameLabel.pack()
 		closeButton.place(x=300,y=270)
 
+		self.imagePanel.pack()
 
 
 	def selectfile(self):
 		self.filename = filedialog.askopenfilename()
 		self.selectedFileName.set(self.filename)
 		return True
+
+	def displayimage(self):
+		img2 = Image.open(self.filename)
+		img2 = img2.resize((120,200), Image.ANTIALIAS)
+		img2 = ImageTk.PhotoImage(img2)
+		self.imagePanel.configure(image=img2)
+		self.imagePanel.image=img2
+		return True
+
+
 		
-
-
-
-
-
 root = Tk()
 my_gui = LeafRecognition(root)
 root.mainloop()
